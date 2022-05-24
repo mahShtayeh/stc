73f874c88a5ed3ea281b887e05cc7ed86882492a
Mahmoud_Shtayeh <mahmoud.shetia@satra.io>
2022-05-24 23:42:06 +0200

    Declare Employee entity

---
+++ employee/src/main/java/com/stc/employee/entities/Employee.java
@@ -0,0 +1,13 @@
+package com.stc.employee.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Employee {
+    private String id;
+    private String name;
+    private String email;
+    private String phone;
+}
