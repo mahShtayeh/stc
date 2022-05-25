8d26aa36a8596f97a3bb788889f51094a532ea6d
Mahmoud_Shtayeh <mahmoud.shetia@satra.io>
2022-05-25 19:47:38 +0200

    Make Employee a jpa entity

--- employee/src/main/java/com/stc/employee/entities/Employee.java
+++ employee/src/main/java/com/stc/employee/entities/Employee.java
@@ -1,13 +1,25 @@
 package com.stc.employee.entities;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+
+import javax.persistence.*;
 
 @Setter
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Employee {
+    @Id
+    @SequenceGenerator(
+            name = "employee_id_sequence",
+            sequenceName = "employee_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "employee_id_sequence"
+    )
     private String id;
     private String name;
     private String email;
